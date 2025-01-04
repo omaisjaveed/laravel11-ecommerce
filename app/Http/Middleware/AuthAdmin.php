ae0389286711b2f16340cd8fa16b1f1d4a1cc06d
omaisjaveed <Omaisjaveed7@gmail.com>
2025-01-04 12:37:07 +0500

    laravel11 updates

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\Auth;
+use Illuminate\support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    
+    {
+
+        if(Auth::check()){
+            if(Auth::user()->utype === 'ADM')
+            {
+                return $next($request);
+            }
+            else{
+                Session::flush();
+                return redirect()->route('login');
+            }
+            
+        }
+        else{
+            return redirect()->route('login');
+        }
+
+
+        return $next($request);
+    }
+}
