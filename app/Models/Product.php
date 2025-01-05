6e27fd1f1075ae88f845dfb46e96c98a1e418610
omaisjaveed <Omaisjaveed7@gmail.com>
2025-01-05 23:57:46 +0500

    realtionships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function category(){
+        return $this->belongsTo(category::class, 'category_id');
+    }
+
+    public function brand(){
+        return $this->belongsTo(Brand::class,'brand_id');
+    }
 }
