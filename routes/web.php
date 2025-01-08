f17183d9385c575dc261396bc2bf3ba438361541
omaisjaveed <Omaisjaveed7@gmail.com>
2025-01-08 14:57:56 +0500

    delete cart

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/shop/{product_slug}',[ShopController::class,'product_details'])->na
 
 Route::get('/cart',[CartController::class,'index'])->name('cart.index');
 Route::post('/cart/add', [CartController::class, 'add_to_cart'])->name('cart.add');
+Route::delete('/cart/remove/{rowId}', [CartController::class, 'remove_item'])->name('cart.item.remove');
 
 Route::put('/cart/increase-quantity/{rowId}', [CartController::class , 'increase_cart_quantity'])->name('cart.qty.increase');
 Route::put('/cart/decrease-quantity/{rowId}', [CartController::class , 'decrease_cart_quantity'])->name('cart.qty.decrease');
