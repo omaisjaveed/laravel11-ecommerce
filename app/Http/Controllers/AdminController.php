419755e7cd3f74930a3034d1465cecb06f0b7838
omaisjaveed <Omaisjaveed7@gmail.com>
2025-01-09 19:35:57 +0500

    Cupon working

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Coupon;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\support\Str;
@@ -412,6 +413,12 @@ class AdminController extends Controller
             }
         }
         return redirect()->route('admin.products')->with('status', 'Product deleted successfully');
+    }
+
+
+    public function coupons(){
+        $coupons = Coupon::orderBy('expiry_date','DESC')->paginate(12);
+        return view('admin.coupons',compact('coupons'));
     }
 
 }
